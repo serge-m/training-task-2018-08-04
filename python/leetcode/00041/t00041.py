5d4e2f8984ecee5325c99a8eec121984b353a772
SergeM <sbmatyunin@gmail.com>
2020-11-21 22:52:49 +0100

    task 41 - first working
    Signed-off-by: SergeM <sbmatyunin@gmail.com>

--- python/leetcode/00041/t00041.py
+++ python/leetcode/00041/t00041.py
@@ -1,48 +1,33 @@
 from typing import List
 
-
-def move_negative_to_end(nums):
-    i_end = len(nums) - 1
-    for i in range(len(nums) - 1, -1, -1):
-        if nums[i] <= 0:
-            nums[i], nums[i_end] = nums[i_end], nums[i]
-            i_end -= 1
-    return i_end + 1, nums
-
-
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        i_end, nums = move_negative_to_end(nums)
-        if i_end == 0:
+        min_positive = min((x for x in nums if x > 0), default=0)
+        if min_positive != 1:
             return 1
-        min_num = min(nums[:i_end])
-        if min_num > 1:
-            return 1
-        for i in range(i_end):
-            while nums[i] != i + 1:
-                cur = nums[i]
-                pos_for_cur = cur - 1
-                if pos_for_cur < i:
 
-                if pos_for_cur < 0 or pos_for_cur >= len(nums):
-                    return i + 1
+        sort1(nums)
+        for i in range(len(nums)):
+            if nums[i] != i + 1:
+                return i + 1
 
-                nums[i], nums[pos_for_cur] = nums[pos_for_cur], nums[i]
-
-        return i_end + 1
+        return len(nums) + 1
 
 
-def test_1():
-    i_end, res = move_negative_to_end([-2, 2, 3, 0, -1])
-    assert set(res[:i_end]) == {2, 3}
-    i_end, res = move_negative_to_end([4, 5])
-    assert set(res[:i_end]) == {4, 5}
-    i_end, res = move_negative_to_end([0, 4, 5])
-    assert set(res[:i_end]) == {4, 5}
-    i_end, res = move_negative_to_end([4, -1, 5])
-    assert set(res[:i_end]) == {4, 5}
-    i_end, res = move_negative_to_end([4, 4, -1, 5])
-    assert sorted(res[:i_end]) == [4, 4, 5]
+def sort1(nums, ):
+    for i in range(len(nums)):
+        while nums[i] != i + 1:
+            if nums[i] <= 0 or nums[i] >= len(nums):
+                break
+            pos_for_cur = nums[i] - 1
+            assert pos_for_cur >= 0
+            if pos_for_cur >= len(nums):
+                return
+            if nums[i] == nums[pos_for_cur]:
+                nums[i] = -1
+                break
+            nums[i], nums[pos_for_cur] = nums[pos_for_cur], nums[i]
+
 
 
 def test_2():
@@ -65,3 +50,11 @@ def test_6():
     assert Solution().firstMissingPositive([1, 1, 3]) == 2
     assert Solution().firstMissingPositive([1, 1, 2]) == 3
     assert Solution().firstMissingPositive([1, 1]) == 2
+
+
+def test_7():
+    assert Solution().firstMissingPositive([0, -1, 3, 1]) == 2
+
+
+def test_8():
+    assert Solution().firstMissingPositive([0, 2, 2, 1, 1]) == 3
