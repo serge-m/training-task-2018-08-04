7683d4641c6fb9fd858456279ab43f03b12b166b
SergeM <sbmatyunin@gmail.com>
2020-11-21 10:42:36 +0100

    task 30 - naming, cleanup
    Signed-off-by: SergeM <sbmatyunin@gmail.com>

--- python/leetcode/00030/t00030.py
+++ python/leetcode/00030/t00030.py
@@ -1,64 +1,81 @@
 import copy
 from collections import defaultdict, Counter
-from typing import List
+from typing import List, Dict
 
 
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         len_word = len(words[0])
         return sum(
-            [
+            (
                 [
                     found + start
                     for found in find(s[start:], words)
                 ]
                 for start in range(0, len_word)
-            ],
+            ),
             []
         )
 
 
 class Matcher:
-    def __init__(self, id_counts):
-        self.id_counts = id_counts
+    """
+    Matches a sliding window of integer sequence to a given multiset of integers.
+    Multiset is represented as dictionary with counts.
+    Sliding window is represented as `add` and `remove` operations that have to be applied
+    by the user as the window moves along the sequence.
+    """
+    def __init__(self, target_counts: Dict[int, int]):
+        self.target_counts = target_counts
+        self.target_sum = sum(self.target_counts.values())
         self.current_counts = defaultdict(int)
+        self.current_sum = 0
 
-    def add(self, word_id):
-        self.current_counts[word_id] += 1
+    def add(self, word: int):
+        self.current_counts[word] += 1
+        self.current_sum += 1
 
-    def remove(self, word_id):
-        self.current_counts[word_id] -= 1
+    def remove(self, word: int):
+        self.current_counts[word] -= 1
+        self.current_sum -= 1
 
     def full(self):
-        if sum(self.current_counts.values()) != sum(self.id_counts.values()):
+        if sum(self.current_counts.values()) != self.target_sum:
             return False
 
-        for word_id, cnt in self.id_counts.items():
-            if self.current_counts[word_id] != cnt:
+        for word, cnt in self.target_counts.items():
+            if self.current_counts[word] != cnt:
                 return False
 
         return True
 
 
 def find(s: str, words: List[str]):
-    len_word = len(words[0])
-    word_to_id, id_counts = build_word_counts(words)
+    cnt_words = len(words)
+    len_word = len(words[0])  # assuming all the words are of the same length
+    word_to_id, id_counts = build_word_counts(words)  # can be extracted to a caller.
 
-    translated = [
+    sequence = [
         word_to_id.get(s[pos: pos + len_word], -1)
         for pos in range(0, len(s), len_word)
     ]
 
-    matcher = Matcher(id_counts)
+    found = match_int_sequence(sequence, id_counts, cnt_words)
+
+    return [pos * len_word for pos in found]
+
+
+def match_int_sequence(sequence, target_counts, cnt_words):
+    matcher = Matcher(target_counts)
 
     result = []
-    for pos in range(len(translated)):
-        matcher.add(translated[pos])
-        pos_to_remove = pos - len(words)
+    for pos in range(len(sequence)):
+        matcher.add(sequence[pos])
+        pos_to_remove = pos - cnt_words
         if pos_to_remove >= 0:
-            matcher.remove(translated[pos_to_remove])
+            matcher.remove(sequence[pos_to_remove])
         if matcher.full():
-            result.append((pos - len(words) + 1) * len_word)
+            result.append(pos - cnt_words + 1)
 
     return result
 
@@ -66,14 +83,11 @@ def find(s: str, words: List[str]):
 def build_word_counts(words):
     word_to_id = {}
     id_counts = {}
-    word_count = {}
     for word_id, word in enumerate(words):
         if word not in word_to_id:
             word_to_id[word] = word_id
-            word_count[word] = 1
             id_counts[word_id] = 1
         else:
-            word_count[word] += 1
             id_counts[word_to_id[word]] += 1
 
     return word_to_id, id_counts
@@ -81,14 +95,6 @@ def build_word_counts(words):
 
 def test_find():
     assert find("b.a.a.a.b.a.a.b.a.", ["a.", "a.", "b.", "b."]) == [8]
-
-
-def test_counter():
-    s = Counter(['a', 'b', 'a'])
-    s.subtract('a')
-    assert s == Counter(['a', 'b'])
-    s.subtract('a')
-    assert s == Counter({'b': 1, 'a': 0})
 
 
 def test_1():
