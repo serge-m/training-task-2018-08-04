03f2ca93b1bc4427db1345157f05c61743c4c6e3
SergeM <sbmatyunin@gmail.com>
2020-11-21 19:15:37 +0100

    Revert "task 37 - implemented sets as binary integer masks - slower"
    This reverts commit 3ab7cf98
    
    Signed-off-by: SergeM <sbmatyunin@gmail.com>

--- python/leetcode/00037/t00037.py
+++ python/leetcode/00037/t00037.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 import numpy as np
 
 empty = 0
+all_9 = list(range(1, 10))
 
 
 class Solution:
@@ -39,24 +40,6 @@ class Solution:
         ]
 
 
-def mask_from_val(val):
-    return 2 ** (val - 1) if val != empty else 0
-
-
-def my_set(vals):
-    res = 0
-    for val in vals:
-        res |= mask_from_val(val)
-    return res
-
-
-all_9 = 2 ** 9 - 1
-
-
-def avail_from_busy(busy):
-    return all_9 & (~busy)
-
-
 class Available:
     def __init__(self, in_cols, in_rows, in_sq):
         self.in_cols = in_cols
@@ -66,11 +49,11 @@ class Available:
     @staticmethod
     def from_busy(cols, rows, sq):
         return Available(
-            in_cols=[avail_from_busy(my_set(col)) for col in cols],
-            in_rows=[avail_from_busy(my_set(row)) for row in rows],
+            in_cols=[set(all_9) - set(col) for col in cols],
+            in_rows=[set(all_9) - set(row) for row in rows],
             in_sq=[
                 [
-                    avail_from_busy(my_set(sq[i][j]))
+                    set(all_9) - set(sq[i][j])
                     for j in range(0, 3)
                 ]
                 for i in range(0, 3)
@@ -92,17 +75,13 @@ class Available:
 
 def available_from_state(state):
     in_rows, in_cols, in_sq = state
-    intersection = in_rows & in_cols & in_sq
-    for i in range(1, 10):
-        if intersection & 1:
-            yield i
-        intersection //= 2
+    return in_rows.intersection(in_cols).intersection(in_sq)
 
 
 def state_minus(state, value):
-    mask = mask_from_val(value)
+    value = {value}
     in_rows, in_cols, in_sq = state
-    return in_rows & (~mask), in_cols & (~mask), in_sq & (~mask)
+    return in_rows.difference(value), in_cols.difference(value), in_sq.difference(value)
 
 
 def step_right(pos):
@@ -178,22 +157,7 @@ def test_profiling():
     import cProfile
 
     with cProfile.Profile() as pr:
-        for i in range(50):
+        for i in range(100):
             test_default()
 
     pr.print_stats()
-
-
-def test_my_set():
-    assert my_set([]) == 0
-    assert my_set([1]) == 1
-    assert my_set([2]) == 2
-    assert my_set([3]) == 4
-    assert my_set([4]) == 8
-    assert my_set([1, 2, 4]) == 8 + 2 + 1
-
-
-def test_avail_from_busy():
-    assert avail_from_busy(my_set([1, 2, 3, 4, 5, 6, 7, 8, 9])) == 0
-    assert avail_from_busy(my_set([2, 3, 4, 5, 6, 7, 8, 9])) == 1
-    assert avail_from_busy(my_set([2, 3, 5, 6, 7, 8, 9])) == 1 + 8
