b96464fedb808744fd756511ee2dc7d966578763
SergeM <sbmatyunin@gmail.com>
2020-12-17 18:04:07 +0100

    build procedure

--- ml/20201215_licence_plate/install.sh
+++ ml/20201215_licence_plate/install.sh
@@ -5,15 +5,15 @@ set -e
 git clone https://github.com/AlexeyAB/darknet darknet
 (cd darknet && git checkout -b "a298" "a298f94255a20a3198d80ea512755d9e5dddbf02")
 
-# for training with GPU
-#(cd darknet
-# sed -i 's/OPENCV=0/OPENCV=1/' Makefile
-# sed -i 's/GPU=0/GPU=1/' Makefile
-# sed -i 's/CUDNN=0/CUDNN=1/' Makefile
-# sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
-# )
+# for training with GPU. Comment out if you dont have CUDA.
+(cd darknet
+ sed -i 's/OPENCV=0/OPENCV=1/' Makefile
+ sed -i 's/GPU=0/GPU=1/' Makefile
+ sed -i 's/CUDNN=0/CUDNN=1/' Makefile
+ sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile
+)
 
-(cd darknet && make -j)
+(cd darknet && ./build.sh)
 
 (
   mkdir -p data &&
