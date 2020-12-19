02eb8098ce3d2b76e06b4166bc3b2e707300e7b9
SergeM <sbmatyunin@gmail.com>
2020-12-19 21:57:46 +0100

    task 743
    Signed-off-by: SergeM <sbmatyunin@gmail.com>

---
+++ python/leetcode/00743/t00743.py
@@ -0,0 +1,35 @@
+import heapq
+from collections import defaultdict
+import sys
+
+inf = sys.maxsize
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        adj = defaultdict(defaultdict)
+        for (u, v, w) in times:
+            adj[u][v] = w
+        best = {
+            u: (inf if u != K else 0)
+            for u in range(1, N + 1)
+        }
+        visited = {
+            u: False
+            for u in range(1, N + 1)
+        }
+        q = [(0, K)]
+        while q:
+            (_, u) = heapq.heappop(q)
+            if visited[u]:
+                continue
+            for v, w in adj[u].items():
+                if best[u] + w < best[v]:
+                    best[v] = best[u] + w
+                    heapq.heappush(q, (best[v], v))
+            visited[u] = True
+
+        result = max(best.values())
+        if result == inf:
+            return -1
+        return result
