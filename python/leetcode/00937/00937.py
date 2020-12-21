d2d31ef16fffbcd2fd5992ca2d250eecd5c74231
SergeM <sbmatyunin@gmail.com>
2020-12-21 16:19:25 +0100

    add 00937

---
+++ python/leetcode/00937/00937.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        with_key = [
+            (self.make_key(log, idx), log) for idx, log in enumerate(logs)
+        ]
+        with_key.sort()
+        return [log for key, log in with_key]
+
+    @staticmethod
+    def make_key(log, idx):
+        words = log.split(' ')
+        first_word = words[1]
+        if first_word[0].isdecimal():
+            return 1, idx
+        else:
+            return 0, words[1:]
+
+
+def test_1():
+    logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+    result = Solution().reorderLogFiles(logs)
+    assert result == ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
