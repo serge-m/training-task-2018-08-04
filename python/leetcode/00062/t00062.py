af90e6740d8eae1af473a8cd0fb2063d1ad4de3f
SergeM <sbmatyunin@gmail.com>
2020-12-21 18:23:56 +0100

    convert to python3.8

--- python/leetcode/00062/t00062.py
+++ python/leetcode/00062/t00062.py
@@ -6,10 +6,17 @@ class Solution:
         return count_paths(m, n)
 
 
-@functools.cache
+@functools.lru_cache(maxsize=None)
 def count_paths(m, n):
     if m < 0 or n < 0:
         return 0
     if (m, n) == (1, 1):
         return 1
     return count_paths(m - 1, n) + count_paths(m, n - 1)
+
+
+def test_count_paths():
+    for i in range(1, 10):
+        for j in range(1, 10):
+            print(f"{count_paths(i, j):4d}", end=" ")
+        print()
