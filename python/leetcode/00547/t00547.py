dc223799a737aeae46ed19fda5d0483cc600336e
SergeM <sbmatyunin@gmail.com>
2020-12-22 15:03:47 +0100

    547: visited is flags

--- python/leetcode/00547/t00547.py
+++ python/leetcode/00547/t00547.py
@@ -10,19 +10,17 @@ from typing import List
 class Solution:
     def findCircleNum(self, M: List[List[int]]) -> int:
         n = len(M)
-        circle = [-1 for i in range(n)]
+        visited = [0 for i in range(n)]
         num_circles = 0
         for i in range(n):
-            if circle[i] == -1:
+            if not visited[i]:
                 num_circles += 1
-                visit(i, i, circle, M)
+                visit(i, visited, M)
         return num_circles
 
 
-def visit(cur, color, circle, M):
-    if circle[cur] != -1:
-        return
-    circle[cur] = color
+def visit(cur, visited, M):
+    visited[cur] = True
     for i in range(len(M)):
-        if M[cur][i] == 1:
-            visit(i, color, circle, M)
+        if M[cur][i] == 1 and not visited[i]:
+            visit(i, visited, M)
