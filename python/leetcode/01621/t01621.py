715a3303916abc077fd9981806d9b7bf13fd78e6
SergeM <sbmatyunin@gmail.com>
2020-12-22 11:26:38 +0100

    add initial version of 1621

---
+++ python/leetcode/01621/t01621.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+
+
+class Solution:
+    def numberOfSets(self, n: int, k: int) -> int:
+        return sum([calc(i, k) for i in range(1, n + 1)], 0) % (10 ** 9 + 7)
+
+
+@lru_cache(maxsize=None)
+def calc(n, k):
+    """
+    count number of ways with a segment starting right at n-1"""
+    if n - 1 < k:
+        return 0
+    if n - 1 == k:
+        return 1
+
+    res = 0
+    for total_len in range(1, n):
+        num_gap_variants = total_len
+        res += num_gap_variants * calc(n - total_len, k - 1)
+    return res % (10 ** 9 + 7)
+
+
+
