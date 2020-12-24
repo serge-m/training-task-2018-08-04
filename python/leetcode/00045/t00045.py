1ebcbf1bb16fb0732b909561ebdbd92c54d36b32
SergeM <sbmatyunin@gmail.com>
2020-12-24 20:00:21 +0100

    45 - time limit for 1 test

---
+++ python/leetcode/00045/t00045.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        max_cnt = 10 ** 10
+        jumps = [max_cnt for _ in nums]
+        jumps[0] = 0
+        for i in range(n):
+            new_val = jumps[i] + 1
+            last_jump = min(i + nums[i], n - 1)
+            for j in range(i + 1, last_jump + 1):
+                if jumps[j] > new_val:
+                    jumps[j] = new_val
+                if j == n - 1:
+                    return jumps[n - 1]
+
+        return jumps[n - 1]
