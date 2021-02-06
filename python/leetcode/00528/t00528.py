764ef79ef833a0e12ca35108bc9c40f65a058be6
SergeM <sbmatyunin@gmail.com>
2021-02-06 17:52:55 +0100

    add 528

---
+++ python/leetcode/00528/t00528.py
@@ -0,0 +1,44 @@
+"""
+528. Random Pick with Weight
+Medium
+
+t=45
+
+w[i] > 0
+"""
+import random
+import itertools
+from typing import List
+
+
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.random = random.Random(0)
+        self.w = w
+        self.sum = sum(w)
+        self.csum = list(itertools.accumulate(w))
+        self.m = len(w)
+        self.x = self.random.random() * self.sum / self.m
+        self.cur = 0
+
+    def pickIndex(self) -> int:
+        # print(self.__dict__)
+        while 1:
+            if self.cur == self.m:
+                self.cur = 0
+                self.x = self.random.random() * self.sum / self.m
+            if self.csum[self.cur] >= self.x:
+                break
+            self.cur += 1
+        self.x += self.sum / self.m
+
+        return self.cur
+
+# s = Solution([1,3])
+# for k in range(8):
+#     print(s.pickIndex())
+# print("------")
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
