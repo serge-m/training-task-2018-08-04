9c31014c71e878eb0cc0f79399441d62a7858cd8
SergeM <sbmatyunin@gmail.com>
2021-02-06 14:49:12 +0100

    removed spaces

--- python/leetcode/01423/t01423.py
+++ python/leetcode/01423/t01423.py
@@ -5,18 +5,15 @@ Medium
 t=15
 """
 
-
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
         n = len(cardPoints)
         sum_taken = 0
-        for i in range(n - k, n):
+        for i in range(n-k, n):
             sum_taken += cardPoints[i]
         best_sum_taken = sum_taken
 
-        print(best_sum_taken)
         for take_from_start in range(0, k):
-            sum_taken = sum_taken + cardPoints[take_from_start] - cardPoints[take_from_start + n - k]
+            sum_taken = sum_taken + cardPoints[take_from_start] - cardPoints[take_from_start+n-k]
             best_sum_taken = max(best_sum_taken, sum_taken)
-            print(take_from_start, best_sum_taken, sum_taken)
         return best_sum_taken
