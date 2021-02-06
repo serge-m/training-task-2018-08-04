729d04bd2959d8f56c4c36f90c7b433e9a2423aa
SergeM <sbmatyunin@gmail.com>
2021-02-07 00:35:18 +0100

    python with sorteddict

--- python/leetcode/01438/t01438.py
+++ python/leetcode/01438/t01438.py
@@ -5,7 +5,7 @@ Medium
 # from dataclasses import dataclass
 from typing import List
 
-from sortedcontainers import SortedList
+from sortedcontainers import SortedList, SortedDict
 
 
 # @dataclass
@@ -60,13 +60,29 @@ class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         n = len(nums)
 
-        w = SortedList()
+        w = SortedDict()
         start = 0
         end = 0
+
+        def add(value):
+            if value not in w:
+                w[value] = 0
+            w[value] += 1
+
+        def delete(value):
+            if w[value] == 1:
+                del w[value]
+            else:
+                w[value] -= 1
+
         while end < n:
-            w.add(nums[end])
-            if w[-1] - w[0] > limit:
-                w.remove(nums[start])
+
+            add(nums[end])
+            largest = w.peekitem(index=-1)
+            smallest = w.peekitem(index=0)
+            if largest[0] - smallest[0] > limit:
+                delete(nums[start])
                 start += 1
             end += 1
         return end - start
+
