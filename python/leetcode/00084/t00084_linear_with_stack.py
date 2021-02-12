169d57cce86d171eb820da7aea83b988a8d8b7fc
SergeM <sbmatyunin@gmail.com>
2021-02-12 22:36:05 +0100

    a bit nicer

--- python/leetcode/00084/t00084_linear_with_stack.py
+++ python/leetcode/00084/t00084_linear_with_stack.py
@@ -5,20 +5,23 @@ Hard
 
 """
 
-
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         hs = heights
-        hs.append(0)
         n = len(hs)
         s = [-1]
         best = 0
 
-        for i in range(n):
-            while len(s) > 1 and hs[s[-1]] >= hs[i]:
-                best = max(best, (i - 1 - s[-2]) * hs[s[-1]])
+        def add(i, val):
+            nonlocal best
+            while len(s) > 1 and hs[s[-1]]>= val:
+                best = max(best, (i - 1 - s[-2]) * hs[s[-1]] )
                 s.pop()
             s.append(i)
+
+        for i in range(n):
+            add(i, hs[i])
+        add(n, 0)
             # print("i", i, "s", s)
             # print("best", best)
         # print("main end")
@@ -29,23 +32,3 @@ class Solution:
         # print("2nd end")
         return best
 
-
-"""
-0 1 2 3 4 5 6
-2 2 0 0 3 3
-i=0: 
-s=[-1]
-s=[-1,0]
-
-i=1
-s=[-1,0]
-s=[-1,0]
-best1 = 1*2 = 2
-s=[-1,1]
-
-
-
-
-
-
-"""
