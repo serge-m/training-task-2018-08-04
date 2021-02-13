d4580f9f28eb2ffb4969e0c4910bf0beb8cc0f47
SergeM <sbmatyunin@gmail.com>
2021-02-13 12:51:10 +0100

    add 11 simpler

--- python/leetcode/00011/t00011.py
+++ python/leetcode/00011/t00011.py
@@ -4,25 +4,19 @@ Medium
 
 """
 
-
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         n = len(height)
 
         l = 0
-        r = n - 1
+        r = n-1
         best = 0
         while l < r:
             best = max(best, min(height[l], height[r]) * (r - l))
             if height[l] > height[r]:
                 r -= 1
-            elif height[l] < height[r]:
-                l += 1
             else:
-                if height[l + 1] > height[r - 1]:
-                    l += 1
-                else:
-                    r -= 1
+                l += 1
 
         return best
 
