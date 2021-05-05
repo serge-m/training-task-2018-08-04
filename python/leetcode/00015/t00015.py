821e2f0cd5e34d9686d25b34da1363f036131f5f
SergeM <sbmatyunin@gmail.com>
2021-05-05 22:33:54 +0200

    faster 00015

--- python/leetcode/00015/t00015.py
+++ python/leetcode/00015/t00015.py
@@ -2,8 +2,9 @@
 15. 3Sum
 Medium
 
-O(n^2)
+912 ms	17.7 MB
 """
+
 from typing import List
 
 
@@ -11,12 +12,24 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         n = len(nums)
-        results = set()
+        results = []
         pos = {x: i for i, x in enumerate(nums)}
         for i in range(n):
+            if nums[i] > 0:
+                break
+
+            # eliminate duplicate for the first number
+            if i > 0 and nums[i-1] == nums[i]:
+                continue
+
             for j in range(i + 1, n):
+                # eliminate duplicates for the second number
+                if j > i+1 and nums[j-1] == nums[j]:
+                    continue
+
                 k = pos.get(-(nums[i] + nums[j]))
                 if k is not None and k > j:
-                    results.add((nums[i], nums[j], nums[k]))
+                    results.append([nums[i], nums[j], nums[k]])
 
-        return [list(triplet) for triplet in results]
+        return results
+
