a9845ca07844c3132d88a8413bf554456eff841e
SergeM <serge-m@users.noreply.github.com>
2021-06-11 22:23:08 +0200

    working multithreaded server

--- rust/hello_server/src/lib.rs
+++ rust/hello_server/src/lib.rs
@@ -1,4 +1,5 @@
-use std::thread::{self, JoinHandle, Thread};
+use std::sync::{Arc, Mutex, mpsc};
+use std::thread::{self, JoinHandle};
 
 struct Worker {
     id: usize,
@@ -6,8 +7,12 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+            println!("Worker {} got a job. executing.", id);
+            job();
+        });
         Worker{id, thread}
     }
     
@@ -15,7 +20,10 @@ impl Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -29,16 +37,21 @@ impl ThreadPool {
         assert!(size > 0);
         let mut workers = Vec::with_capacity(size);
 
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool {workers}
+        ThreadPool {workers, sender}
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
-}
+} 
